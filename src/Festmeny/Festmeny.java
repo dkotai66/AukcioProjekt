6ccfe9f65521d39dc990736bb54df367fca58759
Kótai Dániel <dkotai66@gmail.com>
2022-09-28 11:07:56 +0200

    licit resz kesz

--- src/Festmeny/Festmeny.java
+++ src/Festmeny/Festmeny.java
@@ -30,6 +30,52 @@ public class Festmeny {
         return this.stilus;
     }
 
+    public int getLicitekSzama(){
+        return this.licitekSzama;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje(){
+        return this.legutolsoLicitIdeje;
+    }
+
+    public boolean getElkelt() {
+        return elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    public void licit(){
+        if(this.elkelt==true){
+            System.out.println("A festmény már elkelt");
+        }
+        else{
+            if(this.licitekSzama==0){
+                this.legmagasabbLicit=100;
+                this.licitekSzama++;
+                this.legutolsoLicitIdeje=LocalDateTime.now();
+            }
+            else{
+                this.legmagasabbLicit=(this.legmagasabbLicit/100)*10;
+                this.licitekSzama++;
+                this.legutolsoLicitIdeje=LocalDateTime.now();
+            }
+        }
+
+    }
+
+    public void licit(int mertek){
+        if (mertek<10||mertek>100){
+            System.out.println("Hiba!");
+        }
+        else {
+            this.legmagasabbLicit=(this.legmagasabbLicit/100)*mertek;
+            this.licitekSzama++;
+            this.legutolsoLicitIdeje=LocalDateTime.now();
+        }
+    }
+
     @Override
     public String toString() {
         return String.format("Cím: %s, Festő: %s, Stílus: %s", this.cim, this.festo, this.stilus);
