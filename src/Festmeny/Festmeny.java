0413141dc30f46d02a340467828fd19fac263dcd
Kótai Dániel <dkotai66@gmail.com>
2022-09-28 10:34:16 +0200

    Festmeny letrehozva, getFesto letrehozva

--- src/Festmeny/Festmeny.java
+++ src/Festmeny/Festmeny.java
@@ -1,2 +1,33 @@
-package Festmeny;public class Festmeny {
+package Festmeny;
+
+import java.time.LocalDateTime;
+
+public class Festmeny {
+
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+    public Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+        this.licitekSzama = 0;
+        this.legmagasabbLicit = 0;
+        this.legutolsoLicitIdeje = null;
+        this.elkelt = false;
+    }
+
+    public String getFesto(){
+        return this.festo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cím: %s, Festő: %s, Stílus: %s", this.cim, this.festo, this.stilus);
+    }
 }
--- src/Festmeny/Main.java
+++ src/Festmeny/Main.java
@@ -1,2 +1,11 @@
-package Festmeny;public class Main {
+package Festmeny;
+
+public class Main {
+    public static void main(String[] args) {
+        Festmeny f1 = new Festmeny("Vénusz Születése", "Sandro Botticelli", "reneszánsz");
+
+        System.out.println(f1);
+
+        System.out.println(f1.getFesto());
+    }
 }
